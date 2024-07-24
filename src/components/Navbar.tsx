282f1b2d1cef5bd7c5e35242bad8ae6e53dc47a1
b6540304091 <phiromrat.l@ku.th>
2024-07-24 17:03:54 +0700

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div>
       <div className="flex justify-center ">
-        <div className="self-center top-0 w-full max-w-9xl shadow-lg p-3 mb-5 bg-body rounded ">
+        <div className="self-center top-0 w-full max-w-9xl shadow-lg p-3 mb-5 bg-body rounded">
           <div className="flex justify-end items-center text-gray-700">
             {/* <div className="mx-2 my-4 ">
               <ion-icon
@@ -35,7 +35,7 @@ export default function Navbar() {
                 <a href="/aecranking">AEC Ranking</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
-                <a href="#">ติดต่อ</a>
+                <a href="/contact">ติดต่อ</a>
               </li>
               <li className="text-blue-600 hover:text-white hover:bg-blue-600 mx-4 my-1 border-2 border-blue-600 px-2 rounded-2xl ">
                 <a href="#">LogIn</a>
@@ -66,7 +66,7 @@ export default function Navbar() {
                     <a href="#">Contact</a>
                   </li>
                   <li className="text-blue-600 hover:text-white hover:bg-blue-600 mx-4 my-1 border-2 border-blue-600 px-2 rounded-2xl ">
-                    <a href="#">LogIn</a>
+                    <a href="/signin">LogIn</a>
                   </li>
                   <li className="text-white bg-blue-600 px-2 rounded-2xl mx-4 my-1 hover:bg-white hover:text-blue-600 hover:border-2 hover:border-blue-600">
                     <a href="#">SignUp</a>
