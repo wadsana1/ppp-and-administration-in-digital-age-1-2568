975532f30d6e62fe67cd65f1ee017c1e18bc24ea
Keetadear <keetarat21@gmail.com>
2024-10-11 13:01:14 +0700

    frist commit

---
+++ src/app/section5/page.tsx
@@ -0,0 +1,10 @@
+import Section5 from "@/components/sectiondata/Section5";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <Section5 />
+    </div>
+  );
+}
